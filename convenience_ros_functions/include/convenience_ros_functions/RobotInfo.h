5b79293148f3467ea944b0eda8d9542b63ace2fa
Jennifer Buehler <jennifer.e.buehler@gmail.com>
2016-03-08 00:44:49 +0100

    added RobotInfo

---
+++ convenience_ros_functions/include/convenience_ros_functions/RobotInfo.h
@@ -0,0 +1,81 @@
+#ifndef CONVENIENCE_ROS_FUNCTIONS_ROBOTINFO
+#define CONVENIENCE_ROS_FUNCTIONS_ROBOTINFO
+
+#include <ros/ros.h>
+#include <architecture_binding/Thread.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <sensor_msgs/JointState.h>
+#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <sensor_msgs/MultiDOFJointState.h>
+#include <convenience_ros_functions/ROSFunctions.h>
+
+namespace convenience_ros_functions {
+
+/**
+ * A helper class which includes convenience function to access the most recent information
+ * of the robot state. This class is not continuously subscribed to topics. The subscribers
+ * are launched on-demand, so it is suitable if you only need the states now and then, and
+ * not continuously.
+ *
+ * \author Jennifer Buehler
+ */
+class RobotInfo {
+
+    public:
+    /**
+     * \param default_robot_pose_topic the default robot pose topic. This allows convenience
+     *      functions to be called without specifying the topic every time.
+     * \param default_joint_states_topic the default joint states topic. This allows convenience
+     *      functions to be called without specifying the topic every time.
+     */
+    RobotInfo(const std::string& default_robot_pose_topic="/amcl_pose",
+        const std::string& default_joint_states_topic="/joint_states");
+
+    ~RobotInfo();
+
+    geometry_msgs::PoseStamped getCurrentRobotPose(ros::NodeHandle& n);
+    geometry_msgs::PoseStamped getCurrentRobotPose(const std::string& topicName, ros::NodeHandle& n); 
+    
+    geometry_msgs::PoseWithCovarianceStamped getCurrentRobotPoseWithCovariance(ros::NodeHandle& n); 
+    geometry_msgs::PoseWithCovarianceStamped getCurrentRobotPoseWithCovariance(const std::string& topicName, ros::NodeHandle& n); 
+
+    sensor_msgs::JointState getCurrentJointState(ros::NodeHandle& n);
+    sensor_msgs::JointState getCurrentJointState(const std::string& topicName, ros::NodeHandle& n);
+
+    /**
+     * MoveIt likes to have the MultiDOFJointState in the frame specified at configuration
+     * time (e.g. odom) and seems to have trouble converting it. So do it here.
+     * If this frame changes, pass it into "frame_id" parameter.
+     */
+    static sensor_msgs::MultiDOFJointState getVirtualJointState(const geometry_msgs::PoseStamped& robotPose,
+        const std::string& virtualJointName, const std::string& frame_id); 
+
+    /**
+     * get the current robot pose, and get it as a MulitDOFJointState, transformed to the most recent transform available in frame_id
+     */
+    sensor_msgs::MultiDOFJointState getCurrentVirtualJointState(const std::string& poseTopicName,
+        ros::NodeHandle& n, const std::string& virtualJointName, const std::string& frame_id); 
+
+    private:
+    typedef architecture_binding::mutex mutex;
+    typedef architecture_binding::unique_lock<mutex>::type unique_lock;
+
+    bool receivedJointState();
+    void jointStateCallback(const sensor_msgs::JointState& jointstate); 
+    bool receivedRobotPose();
+    void poseCallback(const geometry_msgs::PoseWithCovarianceStamped& msg); 
+
+    mutex poseMutex;
+    bool received_rp;
+    geometry_msgs::PoseWithCovarianceStamped pose;
+
+    mutex jointStateMutex;
+    sensor_msgs::JointState jointState;
+    bool received_js;
+
+    std::string robot_pose_topic;
+    std::string joint_states_topic;
+};
+
+}
+#endif  // CONVENIENCE_ROS_FUNCTIONS_ROBOTINFO
---
+++ convenience_ros_functions/src/RobotInfo.cpp
@@ -0,0 +1,145 @@
+#include <convenience_ros_functions/RobotInfo.h>
+#include <convenience_ros_functions/ROSFunctions.h>
+
+using convenience_ros_functions::RobotInfo;
+using convenience_ros_functions::ROSFunctions;
+
+RobotInfo::RobotInfo(const std::string& _robot_pose_topic,
+    const std::string& _joint_states_topic):
+    robot_pose_topic(_robot_pose_topic),
+    joint_states_topic(_joint_states_topic)
+{}
+
+RobotInfo::~RobotInfo(){}
+
+geometry_msgs::PoseStamped RobotInfo::getCurrentRobotPose(ros::NodeHandle& n) {
+    return getCurrentRobotPose(robot_pose_topic,n);
+}
+
+geometry_msgs::PoseStamped RobotInfo::getCurrentRobotPose(const std::string& topicName, ros::NodeHandle& n) {
+    geometry_msgs::PoseWithCovarianceStamped p=getCurrentRobotPoseWithCovariance(topicName,n);
+    geometry_msgs::PoseStamped ret;
+    ret.header=p.header;
+    ret.pose=p.pose.pose;
+    return ret;
+}
+
+geometry_msgs::PoseWithCovarianceStamped RobotInfo::getCurrentRobotPoseWithCovariance(const std::string& topicName, ros::NodeHandle& n) {
+    ros::Subscriber jsub = n.subscribe(topicName, 1000, &RobotInfo::poseCallback,this);
+    //ROS_DEBUG("Waiting for robot pos to come in.");
+    // Spin on subscription
+    poseMutex.lock();
+    received_rp=false; 
+    poseMutex.unlock();
+
+    ros::Duration spinWait(0.05);
+    while (!receivedRobotPose()) {
+        ros::spinOnce();
+        spinWait.sleep();
+    }
+
+    //ROS_DEBUG("Received pose.");
+    poseMutex.lock();
+    geometry_msgs::PoseWithCovarianceStamped p=pose;
+    poseMutex.unlock();
+    return p;
+}
+
+geometry_msgs::PoseWithCovarianceStamped RobotInfo::getCurrentRobotPoseWithCovariance(ros::NodeHandle& n) {
+    return getCurrentRobotPoseWithCovariance(robot_pose_topic,n);
+}
+        
+sensor_msgs::JointState RobotInfo::getCurrentJointState(const std::string& topicName, ros::NodeHandle& n){
+    jointStateMutex.lock();
+    received_js=false;
+    jointStateMutex.unlock();
+
+    ros::Subscriber jsub = n.subscribe(topicName, 10, &RobotInfo::jointStateCallback,this);
+    //ROS_INFO("Waiting until current joint state arrives...");
+
+    ros::Duration spinWait(0.05);
+    while (!receivedJointState()) {
+        ros::spinOnce();
+        spinWait.sleep();
+    }
+    
+    //ROS_INFO("Joint state received.");
+
+    jointStateMutex.lock();
+    sensor_msgs::JointState js=jointState;
+    jointStateMutex.unlock();
+    return js;
+}
+
+sensor_msgs::JointState RobotInfo::getCurrentJointState(ros::NodeHandle& n){
+    return getCurrentJointState(joint_states_topic,n);
+}
+
+
+
+sensor_msgs::MultiDOFJointState RobotInfo::getVirtualJointState(const geometry_msgs::PoseStamped& robotPose, const std::string& virtualJointName, const std::string& frame_id) {
+
+    //ROS_INFO_STREAM("Getting virtual joint state for robot pose "<<robotPose);
+
+    geometry_msgs::PoseStamped _robotPose=robotPose;    
+    _robotPose.header.stamp=ros::Time(0); //get the earliest possible transform, so set the time to 0
+    if (_robotPose.header.frame_id!=frame_id) {
+        int ret=ROSFunctions::Singleton()->transformPose(_robotPose,frame_id,_robotPose,2,true);
+        if (ret!=0){
+            ROS_WARN("getVirtualJointState(): Failed to transform robot pose into %s.",frame_id.c_str());
+        }
+    }
+
+
+    sensor_msgs::MultiDOFJointState j;
+    j.header=_robotPose.header;
+    //j.header.stamp=ros::Time::now();
+    //j.header.frame_id=_robotPose.header.frame_id;
+    j.joint_names.push_back(virtualJointName);
+    geometry_msgs::Transform t;
+    t.translation.x=_robotPose.pose.position.x;
+    t.translation.y=_robotPose.pose.position.y;
+    t.translation.z=_robotPose.pose.position.z;
+    t.rotation=_robotPose.pose.orientation;
+    j.transforms.push_back(t);
+
+    //calculate theta from quaternion
+    /*Eigen::Quaterniond targetOri;
+    tf::quaternionMsgToEigen (_robotPose.pose.pose.orientation, targetOri);
+    Eigen::Vector3d targetVector=targetOri*origVector;
+    double dist = angularDistanceInXYSigned(origVector,targetVector);
+    j.position.push_back(dist);
+    */
+    return j;
+
+}
+
+sensor_msgs::MultiDOFJointState RobotInfo::getCurrentVirtualJointState(const std::string& poseTopicName, ros::NodeHandle& n, const std::string& virtualJointName, const std::string& frame_id) {
+    
+    geometry_msgs::PoseStamped robPos=getCurrentRobotPose(poseTopicName, n);
+    return getVirtualJointState(robPos,virtualJointName,frame_id);
+}
+
+bool RobotInfo::receivedJointState(){
+    unique_lock lock(jointStateMutex);
+    return received_js;
+}
+
+void RobotInfo::jointStateCallback(const sensor_msgs::JointState& jointstate) {
+    unique_lock lock(jointStateMutex);
+    jointState=jointstate;
+    received_js=true;
+}
+
+bool RobotInfo::receivedRobotPose(){
+    unique_lock lock(poseMutex);
+    return received_rp;
+}
+
+void RobotInfo::poseCallback(const geometry_msgs::PoseWithCovarianceStamped& msg) {
+    unique_lock lock(poseMutex);
+    //ROS_INFO_STREAM("got current pose: "<<msg.pose.pose.position.x<<", "<<msg.pose.pose.position.y<<", "<<msg.pose.pose.position.z);
+    pose=msg;
+    received_rp=true;
+}
+
