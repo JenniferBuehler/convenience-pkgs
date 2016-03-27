3a6aa719ab47153332c421fe0a1336e4253c7f4b
Jennifer Buehler <jennifer.e.buehler@gmail.com>
2016-03-27 03:47:32 +0200

    update

--- convenience_ros_functions/include/convenience_ros_functions/ROSFunctions.hpp
+++ convenience_ros_functions/include/convenience_ros_functions/ROSFunctions.hpp
@@ -7,23 +7,27 @@ bool ROSFunctions::saveToFile(const ROSMessage& msg, const std::string& filename
     if (asBinary) mode = std::ios::out | std::ios::binary;
     else mode = std::ios::out;
 
-    std::ofstream ofs(filename, mode);
+    std::ofstream ofs(filename.c_str(), mode);
 
-    if (ofs.is_open())
+    if (!ofs.is_open())
     {
         ROS_ERROR("File %s cannot be opened.", filename.c_str());
         return false;
     }
 
+    if (asBinary)
+    {
+        uint32_t serial_size = ros::serialization::serializationLength(msg);
+        boost::shared_array<uint8_t> obuffer(new uint8_t[serial_size]);
+        ros::serialization::OStream ostream(obuffer.get(), serial_size);
+        ros::serialization::serialize(ostream, msg);
+        ofs.write((char*) obuffer.get(), serial_size);
+    }
+    else
+    {
+        ofs<<msg; 
+    }
 
-    uint32_t serial_size = ros::serialization::serializationLength(msg);
-
-    boost::shared_array<uint8_t> obuffer(new uint8_t[serial_size]);
-
-    ros::serialization::OStream ostream(obuffer.get(), serial_size);
-    ros::serialization::serialize(ostream, msg);
-
-    ofs.write((char*) obuffer.get(), serial_size);
     ofs.close();
     return true;
 }
@@ -36,7 +40,7 @@ bool ROSFunctions::readFromFile(const std::string& filename, ROSMessage& msg, bo
     if (isBinary) mode = std::ios::in | std::ios::binary;
     else mode = std::ios::in;
 
-    std::ifstream ifs(filename, mode);
+    std::ifstream ifs(filename.c_str(), mode);
 
     if (!ifs.is_open())
     {
